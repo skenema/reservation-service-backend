b90adc592c85adcbf86689150db1e12c715560f9
pontakornth <pontakorn61@gmail.com>
2023-04-13 11:37:44 +0700

    Add support for multiple showtimes in a movie

--- reservation_service/urls.py
+++ reservation_service/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('reservation/movie=<int:movie>/showtime', views.get_seat),
+    path('reservation/<int:movie_id>/<int:showtime_id>', views.get_seat), # TODO: Change to individual showtime ID.
+    path('reservation/<int:movie_id>/showtimes', views.get_all_showtimes)
 ]
--- reservation_service/views.py
+++ reservation_service/views.py
@@ -6,31 +6,32 @@ from rest_framework import status
 from django.core.exceptions import ObjectDoesNotExist
 
 @api_view(['GET', 'POST'])
-def get_seat(request, movie):
+def get_seat(request, movie_id, showtime_id):
     if request.method == 'GET':
         try:
-            showtime_object = Showtime.objects.get(movie_id=movie)  
+            showtime_object = Showtime.objects.get(id=showtime_id, movie_id=movie_id)
+            seat_object = Seat.objects.filter(showtime_id=showtime_object)
+            # While GET request should not create a resource, it is for development only.
+            # We will need to fix it later.
+            if not seat_object:
+                for i in range(10):
+                    Seat.objects.create(showtime_id=showtime_object,
+                                        seat_id=i + 1,
+                                        is_available=True)
+            array_seat = []
+            for i in range(len(seat_object)):
+                array_seat.append({"seat_id": seat_object[i].seat_id,
+                                   "is_available": seat_object[i].is_available
+
+                                   })
+            return Response(array_seat)
         except ObjectDoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND) 
 
-        showtime_object = Showtime.objects.get(movie_id=movie)  
-        seat_object = Seat.objects.filter(showtime_id = showtime_object)
-        if not seat_object:
-            for i in range(10):
-                Seat.objects.create(    showtime_id = showtime_object,
-                                        seat_id = i+1,
-                                        is_available = True)
-        array_seat = []
-        for i in  range(len(seat_object)):
-            array_seat.append({     "seat_id": seat_object[i].seat_id,
-                                    "showtime_id": seat_object[i].is_available
-
-            })
-        return Response(array_seat)
     elif request.method == 'POST':
         data = request.data
         for i in data["seat_id"]:
-            showtime_object = Showtime.objects.get(movie_id=movie)   
+            showtime_object = Showtime.objects.get(movie_id=movie_id)
             seat_object = Seat.objects.get(  showtime_id = showtime_object, 
                                             seat_id = i)
             if seat_object.is_available == False:
@@ -38,7 +39,7 @@ def get_seat(request, movie):
         
         array_seat = []
         for i in data["seat_id"]:
-            showtime_object = Showtime.objects.get(movie_id=movie)   
+            showtime_object = Showtime.objects.get(movie_id=movie_id)
             seat_object = Seat.objects.filter(  showtime_id = showtime_object, 
                                             seat_id = i).update(is_available = False)
             response = {"seat_ticket": i,
@@ -46,7 +47,15 @@ def get_seat(request, movie):
             array_seat.append(response)
         
         return Response(array_seat)
-        
 
 
-
+@api_view(['GET'])
+def get_all_showtimes(request, movie_id):
+    showtimes = Showtime.objects.filter(movie_id=movie_id).order_by('showtime')
+    showtimes_list = []
+    for showtime in showtimes:
+        showtimes_list.append({
+            'showtime_id': showtime.id,
+            'start_time': showtime.showtime
+        })
+    return Response(showtimes_list)
