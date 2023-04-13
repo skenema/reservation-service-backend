ddd69b3178328c5cf69c76ecad39a3e19e4b928e
pontakornth <pontakorn61@gmail.com>
2023-04-13 11:44:20 +0700

    Make URL more RESTful

--- reservation_service/urls.py
+++ reservation_service/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('reservation/<int:movie_id>/<int:showtime_id>', views.get_seat), # TODO: Change to individual showtime ID.
-    path('reservation/<int:movie_id>/showtimes', views.get_all_showtimes)
+    path('reservation/<int:movie_id>', views.get_all_showtimes),
+    path('reservation/<int:movie_id>/<int:showtime_id>', views.get_seat),
 ]
